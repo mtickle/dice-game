12cfb02e4a83eb81cd2372d63fcd712a95324563
Mike Tickle <mtickle@gmail.com>
2025-06-20 08:23:37 -0400

    Added error boundary.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
+import ErrorBoundary from '@components/ErrorBoundary.jsx'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <ErrorBoundary>
+        <App />
+    </ErrorBoundary>
 )
